7c678d120f3508220d044a6a2db60a4296da26f3
Ebben <abanalhindi47@gmail.com>
2023-11-20 12:10:48 +0100

    naming

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_controller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [Header("||PLAYER CONTROLS||")]
     [SerializeField] float speed;
