eaef10ad2fd019ffb82e3c1bce463bcb45d2745f
boominh <michynuggy@gmail.com>
2023-11-24 12:14:41 +0100

    test

--- Assets/Scripts/world_objects/Trampoline.cs
+++ Assets/Scripts/world_objects/Trampoline.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
+    public bool pushWithBounceForce;
     public float bounceForce = 10f;
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -9,12 +10,17 @@ public class Trampoline : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Rigidbody2D rb2d = other.gameObject.GetComponent<Rigidbody2D>();
+            
+            if (!pushWithBounceForce)
+            {
+                bounceForce = rb2d.gravityScale * 5;
+            }
 
             if (rb2d != null)
             {                
-                bounceForce = rb2d.gravityScale * 5;
-                Vector2 bounceDirection = new Vector2(0f, bounceForce);
-                rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+                Vector2 bounceDirection = transform.up * bounceForce;
+                print(bounceDirection);
+                //rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
                 rb2d.AddForce(bounceDirection, ForceMode2D.Impulse);
 
                 //PlayerController player = rb2d.GetComponent<PlayerController>();
