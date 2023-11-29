0cd686e6ae43b1908929a80457e6c1b88b440077
boominh <michynuggy@gmail.com>
2023-11-29 11:09:54 +0100

    Update Destructible.cs

--- Assets/Scripts/world_objects/Platforms/Destructible.cs
+++ Assets/Scripts/world_objects/Platforms/Destructible.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
+    public GameObject terrainObject;
+    SpriteRenderer objectSpriteRenderer;
+
+    bool ongoingCoroutine;
+
+    public Color32 onTriggerColor = Color.red;
+    private Color32 defaultColor;
+
+    [Header("Respawn")]
+    public bool respawnEnabled = false;
+    public float respawnDelay = 5f;
+
+
     void Start()
     {
-        
+        objectSpriteRenderer = terrainObject.GetComponent<SpriteRenderer>();
+        defaultColor = objectSpriteRenderer.color;
     }
 
-    void Update()
+    public void GetDestroyed()
     {
-        
+        if (!ongoingCoroutine)
+        {
+            StartCoroutine(Demolish());
+        }
+    }
+
+    IEnumerator Demolish()
+    {
+        ongoingCoroutine = true;
+
+        objectSpriteRenderer.color = onTriggerColor;
+
+        terrainObject.SetActive(false);
+
+        if (respawnEnabled)
+        {
+            yield return new WaitForSeconds(respawnDelay);
+            terrainObject.SetActive(true);
+        }
+
+        objectSpriteRenderer.color = defaultColor;
+
+        ongoingCoroutine = false;
     }
 }
