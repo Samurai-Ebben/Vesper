adfbc264c78ab0ebb0c01e69dc03b5da05d77add
Bedirhan233 <bedirhanko73@gmail.com>
2023-12-04 11:54:16 +0100

    CameraFix

--- Assets/Scripts/Player/ScreenShakeHandler.cs
+++ Assets/Scripts/Player/ScreenShakeHandler.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class ScreenShakeHandler : MonoBehaviour
 {
-    PlayerController controller;
+    public float duration = 2;
+    public float strength = 1.0f;
+
+    Vector3 origPos;
+    public bool startTheShake;
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<PlayerController>();
+        origPos = transform.position;
+     
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButton(0))
+        {
+            StartCoroutine(ShakeScreen()); 
+        }
+
+    }
+    IEnumerator ShakeScreen()
+    {
+        float timeElapsed = 0;
         
+
+        while(timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;  
+
+            transform.position = transform.position + Random.insideUnitSphere * strength;
+            yield return null;
+        }
+
+        transform.position = origPos;   
+
+
     }
 }
