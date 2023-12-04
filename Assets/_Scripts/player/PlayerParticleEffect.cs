e725075d5f3cff159e2c7f6869a65b2c72cf3f58
Ebben <abanalhindi47@gmail.com>
2023-12-04 11:59:12 +0100

    particleEffectPlayer

--- Assets/_Scripts/player/PlayerParticleEffect.cs
+++ Assets/_Scripts/player/PlayerParticleEffect.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerParticleEffect : MonoBehaviour
 {
 
-    private ParticleSystem pfx;
+    public ParticleSystem pfx;
+    public ParticleSystem trail;
+    public ParticleSystem jumpFx;
+    public ParticleSystem landFx;
     private PlayerController player;
-    public float sizeOffset;
+    public float sizeOffset = 1.2f;
+    public float trailOffset = 1.2f;
 
+    public bool isJump = false;
+    public bool isLand = false;
     private void Start()
     {
-        player = GetComponent<PlayerController>();
-        pfx = GetComponentInChildren<ParticleSystem>();
+        player  = GetComponent<PlayerController>();      
     }
 
     private void Update()
@@ -23,6 +28,16 @@ public class PlayerParticleEffect : MonoBehaviour
     private void ParticleSizeToPlayer()
     {
         ParticleSystem.ShapeModule pfxShape = pfx.shape;
+        ParticleSystem.ShapeModule trailShape = trail.shape;
+        trailShape.scale = player.transform.localScale * trailOffset;
         pfxShape.scale = player.transform.localScale * sizeOffset;
+    }
+
+    private void PlayEffects()
+    {
+        if(isJump)
+            jumpFx.Play();
+        if(isLand)
+            landFx.Play();
     }
 }
