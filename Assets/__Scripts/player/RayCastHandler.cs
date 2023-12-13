768f3b4f184e4659047ef07b64bf87e03a7bf8a4
Bedirhan233 <bedirhanko73@gmail.com>
2023-12-13 09:48:09 +0100

    UppdateRaycast

--- Assets/__Scripts/player/RayCastHandler.cs
+++ Assets/__Scripts/player/RayCastHandler.cs
@@ -1,108 +1,169 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TarodevController;
 using UnityEngine;
+using static Unity.Collections.AllocatorManager;
 
 public class RayCastHandler : MonoBehaviour
 {
+    BoxCollider2D col;
     PlayerController controller;
-    public float checkingTheTop = 0.25f;
-    float smallPlayerRay = 0.25f;
-    float mediumPlayerRay = 0.50f;
-    float largePlayerRay = 0.75f;
+
+    [Header("RayCast Length")]
+    public float smallRaycastLength = 0.25f;
+    public float mediumRaycastLength = 0.50f;
+    public float largeRaycastLength = 0.75f;
+
+    [Header("Total Raycast")]
+    public int totalTopRaycast = 3;
+    public int totalSideRaycast = 3;
+    public int totalDownRaycast = 3;
+    int totalDiagonalRaycast = 1;
+
+
 
     public float sideCheckLength;
     public float diagonalLength = 0.5f;
 
     //since this is ground, maybe rename it?
-    public LayerMask isBlock;
+    public LayerMask mask;
 
     float drawRay = 1;
 
+    RaycastHit2D centerRaycast;
     public float drawRayForMedium;
     public float drawRayForLarge;
     public float drawRayForSmall;
 
-    public bool mediumCanChangeSize;
-    public bool largeCanChangeSize;
-    public bool smallCanChangeSize;
+    public bool mediumTopIsFree;
+    public bool largeTopIsFree;
+    public bool smallTopIsFree;
 
-    public bool sideCheck;
-    public bool diagonalCheck;   
+    public bool rightSide;
+    public bool leftSide;
+    public bool anySide;
 
+    public bool rightTop;
+    public bool rightDown;
+    public bool leftDown;
+    public bool leftTop;
+    public bool diagonalTop;
+
+    public bool smallDownIsFree;
+    public bool mediumDownIsFree;
+    public bool largeDownIsFree;
+
+
+
+    public bool diagonalCheck;
+
+
+    public float edgeOffset = 1.5f;
+    float offset;
     // Start is called before the first frame update
     void Start()
     {
         Physics2D.queriesStartInColliders = false;
         controller = GetComponent<PlayerController>();
+        col = GetComponentInChildren<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-            smallCanChangeSize = RayCastGenerator(smallPlayerRay, Vector2.up, Color.red);
-        
-            largeCanChangeSize = RayCastGenerator(largePlayerRay, Vector2.up, Color.red);
-        
-            mediumCanChangeSize = RayCastGenerator(mediumPlayerRay, Vector2.up, Color.red);
+
+        smallTopIsFree = RayCastGenerator(smallRaycastLength, Vector2.up, totalTopRaycast);
+        largeTopIsFree = RayCastGenerator(largeRaycastLength, Vector2.up, totalTopRaycast);
+        mediumTopIsFree = RayCastGenerator(mediumRaycastLength, Vector2.up, totalTopRaycast);
 
 
-            sideCheck = RayCastGenerator(sideCheckLength, Vector2.right, Color.green) || RayCastGenerator(sideCheckLength, Vector2.left, Color.green);
-            diagonalCheck = RayCastGenerator(diagonalLength, new Vector2(-1, 1), Color.green) || RayCastGenerator(diagonalLength, new Vector2(1, 1), Color.red);
+        rightSide = RayCastGenerator(sideCheckLength, Vector2.right, totalSideRaycast);
+        leftSide = RayCastGenerator(sideCheckLength, Vector2.left, totalSideRaycast);
+        anySide = leftSide || rightSide;
+
+        RayCastGenerator(smallRaycastLength, Vector2.down, totalDownRaycast);
+        RayCastGenerator(largeRaycastLength, Vector2.down, totalDownRaycast);
+        RayCastGenerator(mediumRaycastLength, Vector2.down, totalDownRaycast);
+
+
+
+        rightTop = RayCastGenerator(diagonalLength, new Vector2(1, 1), totalDiagonalRaycast);
+        rightDown = RayCastGenerator(diagonalLength, new Vector2(1, -1), totalDiagonalRaycast);
+        leftDown = RayCastGenerator(diagonalLength, new Vector2(-1, -1), totalDiagonalRaycast);
+        leftTop = RayCastGenerator(diagonalLength, new Vector2(-1, 1), totalDiagonalRaycast);
+        diagonalTop = rightTop || leftTop;
+
+        Debug.Log(rightSide);
 
 
         //Make this function more generic and better to read please.
-   
+
 
     }
-    bool RayCastGenerator(float characterSize, Vector2 direction, Color rayColor)
+    bool RayCastGenerator(float characterSize, Vector2 direction, int totalRaycast)
     {
-        bool canChangeSize;
-        if (characterSize == mediumPlayerRay)
+
+        Vector3 center = new Vector3(transform.position.x, transform.position.y);
+
+        bool canChangeSize = true;
+
+        for (int i = 0; i < totalRaycast; i++)
         {
-            drawRay = drawRayForMedium;
-        }
+            if (direction == Vector2.up || direction == Vector2.down)
+            {
+                offset = (col.bounds.size.x * edgeOffset) / totalRaycast;
+                centerRaycast = Physics2D.Raycast(center + new Vector3((i - (totalRaycast - 1) / 2f) * offset, 0), direction, characterSize, mask);
+                Debug.DrawRay(center + new Vector3((i - (totalRaycast - 1) / 2f) * offset, 0), direction * characterSize, Color.red);
+            }
 
-        if (characterSize == smallPlayerRay)
-        {
-            drawRay = drawRayForSmall;
-        }
-
-        if (characterSize == largePlayerRay)
-        {
-            drawRay = drawRayForLarge;
-        }
-
-        float leftOffset = transform.position.x - drawRay;
-        Vector3 left = new Vector3(leftOffset, transform.position.y);
-
-
-        float rightOffset = transform.position.x + drawRay;
-        Vector3 right = new Vector3(rightOffset, transform.position.y);
-
-
-
-        RaycastHit2D leftTopRay = Physics2D.Raycast(left, direction, characterSize, isBlock);
-        RaycastHit2D rightTopRay = Physics2D.Raycast(right, direction, characterSize, isBlock);
+            else if (direction == Vector2.right || direction == Vector2.left)
+            {
+                offset = (col.bounds.size.y * edgeOffset) / totalRaycast;
+                centerRaycast = Physics2D.Raycast(center + new Vector3(0, (i - (totalRaycast - 1) / 2f) * offset), direction, characterSize, mask);
+                Debug.DrawRay(center + new Vector3(0, (i - (totalRaycast - 1) / 2f) * offset), direction * characterSize, Color.red);
+            }
+            else if (direction == new Vector2(1, 1) || direction == new Vector2(1, -1) || direction == new Vector2(-1, 1) || direction == new Vector2(-1, -1))
+            {
+                offset = (col.bounds.size.y * edgeOffset) / totalRaycast;
+                centerRaycast = Physics2D.Raycast(center + new Vector3((i - (totalRaycast - 1) / 2f), (i - (totalRaycast - 1) / 2f) * offset), direction, characterSize, mask);
+                Debug.DrawRay(center + new Vector3((i - (totalRaycast - 1) / 2f), (i - (totalRaycast - 1) / 2f) * offset), direction * characterSize, Color.red);
+            }
 
 
-        // for debug and colors
-        if (rayColor != Color.red)
-        {
-            Debug.DrawRay(left, direction * characterSize, rayColor);
-            Debug.DrawRay(right, direction * characterSize, rayColor);
+            canChangeSize = centerRaycast.collider == null;
 
-        }
-        else
-        {
-            Debug.DrawRay(left, direction * characterSize, Color.red);
-            Debug.DrawRay(right, direction * characterSize, Color.red);
+            if (centerRaycast.collider != null)
+            {
+                canChangeSize = false;
+                break;
+            }
 
         }
-
-        canChangeSize = leftTopRay.collider == null && rightTopRay.collider == null;
-        //Debug.Log(canChangeSize);
         return canChangeSize;
+
+
+
     }
+
+    //private void DirectionHandler(float characterSize, Vector2 direction, int totalRaycast, Vector3 center, int i)
+    //{
+    //    Vector3 offsetBetweenRay = Vector2.zero;
+
+    //    if (direction == Vector2.up || direction == Vector2.down)
+    //    {
+    //        offset = (col.bounds.size.x * edgeOffset) / totalRaycast;
+    //        offsetBetweenRay = new Vector2((i - (totalRaycast - 1) / 2f) * offset, 0);
+    //    }
+    //    else if (direction == Vector2.right || direction == Vector2.left)
+    //    {
+    //        offset = (col.bounds.size.y * edgeOffset) / totalRaycast;
+    //        offsetBetweenRay = new Vector2(0, ((i - totalRaycast - 1) / 2f) * offset);
+    //    }
+
+
+    //    centerRaycast = Physics2D.Raycast(center + offsetBetweenRay, direction, characterSize, mask);
+    //    Debug.DrawRay(center + offsetBetweenRay, direction * characterSize, Color.red);
+    //}
 }
