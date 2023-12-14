803f7f90aec45b07c29a4e5bd4a949c1d24adb81
boominh <michynuggy@gmail.com>
2023-12-14 09:40:11 +0100

    Collectible collider to trigger

--- Assets/__Scripts/Environment/Collectible/Collectible.cs
+++ Assets/__Scripts/Environment/Collectible/Collectible.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
-{
-    private void OnCollisionEnter2D(Collision2D collision)
+{    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             var collectibleManager = GameManager.Instance.GetComponent<CollectibleManager>();
 
