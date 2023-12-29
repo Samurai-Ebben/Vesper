fc9ab6c8ffb6ddc1df87f574cce09c6f18b4933c
Bedirhan233 <bedirhanko73@gmail.com>
2023-12-29 09:34:25 +0100

    debug

--- Assets/__Scripts/player/PlayerAudioHandler.cs
+++ Assets/__Scripts/player/PlayerAudioHandler.cs
@@ -23,7 +23,6 @@ public class PlayerAudioHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(AudioManager.Instance);
     }
 
     public void PlayLandingSound()
