6faa4549404cab2458f40d407f402fe7f4cc9824
Ebben <abanalhindi47@gmail.com>
2023-12-29 17:06:06 +0100

    Update MenuManager.cs

--- Assets/__Scripts/GameManager/MenuManager.cs
+++ Assets/__Scripts/GameManager/MenuManager.cs
@@ -43,7 +43,7 @@ public class MenuManager : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
     public void NavigateBtns()
     {
