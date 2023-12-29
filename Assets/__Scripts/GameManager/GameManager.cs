c7f42c3deb4bb616fafe29014f9dc6e27b465652
Bedirhan233 <bedirhanko73@gmail.com>
2023-12-29 09:45:46 +0100

    fixedSound

--- Assets/__Scripts/GameManager/GameManager.cs
+++ Assets/__Scripts/GameManager/GameManager.cs
@@ -67,7 +67,6 @@ public class GameManager : MonoBehaviour
     
     public void Death()
     {
-        //Debug.Log(AudioManager.Instance);
         AudioManager.Instance.PlayingAudio(AudioManager.Instance.death, AudioManager.Instance.deathVolume);
         PlayerController.instance.canMove = false;
         PlayerController.instance.effects.enabled = true;
