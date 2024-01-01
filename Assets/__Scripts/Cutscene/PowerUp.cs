6cb3cd803006da3eeb10087520576ce06889a938
Ebben <abanalhindi47@gmail.com>
2024-01-01 16:06:37 +0100

    powerUp anim

--- Assets/__Scripts/Cutscene/PowerUp.cs
+++ Assets/__Scripts/Cutscene/PowerUp.cs
@@ -29,20 +29,38 @@ public class PowerUp : MonoBehaviour
     [Header("Animation")]
     public bool playPlayerAnimation = true;
     public bool playObjectsAnimation = true;
-
+    public float beatingDuration = 1;
+    [Range(0, 2)] public float sizeMulti = 1.02f;
+    float pulsDuration = 1;
+    float pulsingTimer = 0;
     public float animationDuration;
     public List<GameObject> animationObjects;
 
     FadeSprite fadeSprite;
     Rigidbody2D rb2d;
     bool cutscenePlayed = false;
+    Vector3 origiScale;
+    private bool isSizeChange = false;
 
     void Start()
     {
+        origiScale = transform.localScale;
         fadeSprite = GetComponent<FadeSprite>();
         rb2d = PlayerController.player.GetComponent<Rigidbody2D>();
     }
 
+
+    private void Update()
+    {
+        pulsingTimer += Time.deltaTime;
+
+        if (pulsingTimer >= pulsDuration && !isSizeChange)
+        {
+            isSizeChange = true;
+            Pulsing ();
+            pulsingTimer = 0f;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !cutscenePlayed)
@@ -98,6 +116,14 @@ public class PowerUp : MonoBehaviour
         PlayPlayerAnimation(false);
 
         EnableSize();
+    }
+    public void Pulsing()
+    {
+        var newScale = origiScale * sizeMulti;
+        Sequence sizeSeq = DOTween.Sequence();
+        sizeSeq.Append(transform.DOScale(newScale, beatingDuration / 2).SetEase(Ease.InSine))
+            .Append(transform.DOScale(origiScale, beatingDuration / 2).SetEase(Ease.InSine))
+            .OnComplete(() => isSizeChange = false);
     }
 
     private void EnableSize()
