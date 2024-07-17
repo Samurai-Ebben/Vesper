7608c3289b8eeb464b6abcf5fd32d23c60f45774
Ebben <abanalhindi47@gmail.com>
2024-07-17 22:19:44 +0200

    LevelSelector Done

---
+++ Assets/__Scripts/GameManager/LevelButton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour
+{
+    public int levelIndex;
+    public Sprite collectibleSprite;
+    private TextMeshProUGUI lvlTxt;
+    private Button button;
+    private Image buttonImage;
+
+
+    void Start() {
+        button = GetComponent<Button>();
+        lvlTxt = GetComponentInChildren<TextMeshProUGUI>();
+        buttonImage = GetComponent<Image>();
+        UpdateButton();
+    }
+
+
+
+    public void UpdateButton() {
+        LevelState state = LvlSelectorManager.Instance.levels[levelIndex];
+
+        if (!state.entered) {
+            button.interactable = false;
+        }
+        else if (state.collectibleCollected) {
+            button.interactable = true;
+            lvlTxt.color = Color.black;
+            buttonImage.sprite = collectibleSprite;
+        }
+        else {
+            button.interactable = true;
+        }
+    }
+}
