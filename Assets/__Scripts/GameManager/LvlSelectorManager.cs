a893e2497f0cf57e8bd8b49f20172a25837550be
Abdalrahman Alhindi <abanalhindi47@gmail.com>
2024-07-26 16:29:44 +0100

    Update LvlSelectorManager.cs

--- Assets/__Scripts/GameManager/LvlSelectorManager.cs
+++ Assets/__Scripts/GameManager/LvlSelectorManager.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.EventSystems;
